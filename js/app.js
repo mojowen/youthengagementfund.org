9ebf4809ca1fbb220a121f6eab54f29658b20b84
Scott Duncombe <sduncombe@renewfinancial.com>
2017-05-02 21:19:55 -0700

    Fix form

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 
 function validateAndSubmit(form) {
   var email = form.querySelector('[name=email]').value,
-      confirm = form.querySelector('[name=confirm]').value,
+      confirm = form.querySelector('[name=confirm]').value
 
   if( email === confirm ) return serializerAndSubmit(form)
-
 
   alert('Email and Confirm Email must match')
   return false
@@ -20,9 +19,9 @@ function serializerAndSubmit(form) {
 
   form.querySelector('button, input[type=submit]').setAttribute('disabled', true)
 
-  tinyPOST('https://mail.scottduncombe.com/', data, function(resp) {
-      form.querySelector('button, input[type=submit]').setAttribute('disabled', null)
-      alert('Your message has been sent')
+  tinyPOST('https://mail.scottduncombe.com/', data, function(resp, error) {
+    form.querySelector('button, input[type=submit]').setAttribute('disabled', null)
+    alert(error ? 'Something has gone wrong - please try again' : 'Your message has been sent')
   })
 
   return false
